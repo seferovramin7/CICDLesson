276268d7dd6a6e22e3965612e4a8f703c77133af
Mohammad Milzon <106896186+Milzon1010@users.noreply.github.com>
2025-08-30 14:54:16 +0000

    Add HelloController with root and /api/hello endpoint

--- src/main/java/com/ltclab/cicdlesson/HelloController.java
+++ src/main/java/com/ltclab/cicdlesson/HelloController.java
@@ -2,12 +2,18 @@ package com.ltclab.cicdlesson;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
+    public String helloRoot() {
         return "Hellooooo, World!";
+    }
+
+    @GetMapping("/api/hello")
+    public Map<String, String> helloApi() {
+        return Map.of("message", "Hello from Spring Boot + Codespaces!");
     }
 }
